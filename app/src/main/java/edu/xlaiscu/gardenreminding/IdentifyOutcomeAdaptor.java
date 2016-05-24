a29b2e5c5ac91a8367ed30f62e4c42cfa6d259fa
laixiaoyuan <Lexie@Xiaoyuans-Air.lan>
2016-05-24 01:56:12 -0700

    we might need to download and store all pictures in advance

--- app/src/main/java/edu/xlaiscu/gardenreminding/IdentifyOutcomeAdaptor.java
+++ app/src/main/java/edu/xlaiscu/gardenreminding/IdentifyOutcomeAdaptor.java
@@ -2,6 +2,8 @@ package edu.xlaiscu.gardenreminding;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,8 @@ import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.net.URL;
 
 /**
  * Created by Lexie on 5/22/16.
@@ -30,6 +34,9 @@ public class IdentifyOutcomeAdaptor extends CursorAdapter{
         String photoPath = cursor.getString(cursor.getColumnIndex("photoPath"));
 
         ((TextView) view.findViewById(R.id.txtNote)).setText(plantName);
+
+//        URL newurl = new URL(photoPath);
+//        Bitmap myIcaon_val = BitmapFactory.decodeStream(newurl.openConnection().getInputStream());
         ((ImageView) view.findViewById(R.id.icon)).setImageURI(Uri.parse(photoPath));
     }
 }
